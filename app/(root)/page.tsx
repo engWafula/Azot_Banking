1e7acc6d2235cd9a3b5eb595c92706df49e9a87b
engWafula <wafulaallan5@gmail.com>
2024-05-04 09:11:39 +0300

    adding home pages layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
 
 function Home() {
+    const loggedIn = {firstName:"Wafula Allan"}
   return (
-    <div>
-         Am home
-    </div>
-  )
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={20000}
+          /> 
+        </header>
+      </div>
+    </section>
+  );
 }
 
-export default Home
+export default Home;
